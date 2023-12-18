464419e8a6d59947f291ceee7db973ea4bf738d7
unknown <antoine_grubert@hotmail.fr>
2023-12-18 13:13:51 +0100

    feat : fin de la page profil

--- src/components/ProfileSeenList.tsx
+++ src/components/ProfileSeenList.tsx
@@ -1,9 +1,46 @@
+import styled from "styled-components"
+import { spacing, mediaSizes } from "../data/styleVariables"
 
+const UserSeenListContainer = styled.section`
+    margin-bottom: calc(${spacing} * 5);
+`
+
+const UserSeenListTitle = styled.h2`
+    display:inline-block;
+    margin-bottom: calc(${spacing} * 2.5);
+    border-bottom: 1px solid white;
+
+    i {
+        color:white;
+        cursor: pointer;
+        font-size:1rem;
+    }
+`
+
+const UserSeenList = styled.div`
+    display: flex;
+    gap: 10px;
+    flex-wrap: wrap;
+    @media (min-width: ${mediaSizes.tablet}) {
+        gap: 30px;
+    }
+
+    img {
+        width: clamp(100px, 30%, 200px);
+    }
+`
 
 const ProfileSeenList = () => {
     return (
         <>
-
+            <UserSeenListContainer>
+                <UserSeenListTitle> J'ai vu ces films <i className="fa-solid fa-chevron-down"></i> </UserSeenListTitle>
+                <UserSeenList>
+                    <img src="/tests/inception-affiche.jpg" alt="affiche" />
+                    <img src="/tests/inception-affiche.jpg" alt="affiche" />
+                    <img src="/tests/inception-affiche.jpg" alt="affiche" />
+                </UserSeenList>
+            </UserSeenListContainer>
         </>
     )
 }
--- src/components/ProfileWishedList.tsx
+++ src/components/ProfileWishedList.tsx
@@ -1,16 +1,46 @@
-// import styled from "styled-components"
-// import { spacing } from "../data/styleVariables"
+import styled from "styled-components"
+import { spacing, mediaSizes } from "../data/styleVariables"
 
-// const UserInfosContainer = styled.section`
-//     margin-bottom: calc(${spacing} * 5);;
-// `
+const UserWishedListContainer = styled.section`
+    margin-bottom: calc(${spacing} * 5);
+`
 
+const UserWishedListTitle = styled.h2`
+    display:inline-block;
+    margin-bottom: calc(${spacing} * 2.5);
+    border-bottom: 1px solid white;
 
+    i {
+        color:white;
+        cursor: pointer;
+        font-size:1rem;
+    }
+`
+
+const UserWishedList = styled.div`
+    display: flex;
+    gap: 10px;
+    flex-wrap: wrap;
+    @media (min-width: ${mediaSizes.tablet}) {
+        gap: 30px;
+    }
+
+    img {
+        width: clamp(100px, 30%, 200px);
+    }
+`
 
 const ProfileWishedList = () => {
     return (
         <>
-
+            <UserWishedListContainer>
+                <UserWishedListTitle>Je veux voir ces films <i className="fa-solid fa-chevron-down"></i> </UserWishedListTitle>
+                <UserWishedList>
+                    <img src="/tests/inception-affiche.jpg" alt="affiche" />
+                    <img src="/tests/inception-affiche.jpg" alt="affiche" />
+                    <img src="/tests/inception-affiche.jpg" alt="affiche" />
+                </UserWishedList>
+            </UserWishedListContainer>
         </>
     )
 }
